1566afd67ba058ff04d482f30693ac35114b4788
Kushpriya <priyakushawaha73@gmail.com>
2024-07-31 11:05:21 +0545

    test

---
+++ demo.rb
@@ -0,0 +1,23 @@
+foods = {
+    burger:4,
+    pizza:3,
+    chowmein:2,
+    momo:10
+}
+
+puts "what would you like to do?"
+  puts "if you want to add food then enter 1"
+  puts "to update enter 2"
+  puts "to delete enter 3"
+  puts "to display enter 4"
+  
+ choice = gets.chomp
+ case choice
+ when "1"
+    puts "which food do you want to add?"
+        food_name = gets.chomp.to_sym
+        puts "Give rating to the food?" 
+        rating = gets.chomp.to_i
+        
+
+
